99d1f839831a4d928f470703d88c41e0bd978828
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-04-14 12:12:33 +0200

    Parameterize ca certificate location (#6)
    We have found that is not always at harcoded place, let's parameterize it
    so user can choose different one.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- pkg/webhook/server/server.go
+++ pkg/webhook/server/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
@@ -13,14 +14,16 @@ import (
 )
 
 type Server struct {
-	mgr           manager.Manager
-	webhookName   string
-	webhookType   certificate.WebhookType
-	webhookServer *webhook.Server
-	log           logr.Logger
+	mgr              manager.Manager
+	webhookName      string
+	webhookType      certificate.WebhookType
+	webhookServer    *webhook.Server
+	caConfigMapKey   types.NamespacedName
+	caConfigMapField string
+	log              logr.Logger
 }
 
-type ServerModifier func(w *webhook.Server)
+type ServerModifier func(w *Server)
 
 // Add creates a new Conditions Mutating Webhook and adds it to the Manager. The Manager will set fields on the Webhook
 // and Start it when the Manager is Started.
@@ -32,43 +35,60 @@ func New(mgr manager.Manager, webhookName string, webhookType certificate.Webhoo
 			Port:    8443,
 			CertDir: "/etc/webhook/certs/",
 		},
-		mgr: mgr,
-		log: logf.Log.WithName("webhook/server"),
+		caConfigMapKey: types.NamespacedName{
+			Namespace: "kube-system",
+			Name:      "extension-apiserver-authentication",
+		},
+		caConfigMapField: "client-ca-file",
+		mgr:              mgr,
+		log:              logf.Log.WithName("webhook/server"),
 	}
-	s.UpdateOpts(serverOpts...)
+	s.updateServerOpts(serverOpts...)
 
 	return s
 }
 
 func WithHook(path string, hook *webhook.Admission) ServerModifier {
-	return func(s *webhook.Server) {
-		s.Register(path, hook)
+	return func(s *Server) {
+		s.webhookServer.Register(path, hook)
 	}
 }
 
 func WithPort(port int) ServerModifier {
-	return func(s *webhook.Server) {
-		s.Port = port
+	return func(s *Server) {
+		s.webhookServer.Port = port
 	}
 }
 
 func WithCertDir(certDir string) ServerModifier {
-	return func(s *webhook.Server) {
-		s.CertDir = certDir
+	return func(s *Server) {
+		s.webhookServer.CertDir = certDir
+	}
+}
+
+func WithCaConfigMapKey(key types.NamespacedName) ServerModifier {
+	return func(s *Server) {
+		s.caConfigMapKey = key
+	}
+}
+
+func WithCaConfigMapField(field string) ServerModifier {
+	return func(s *Server) {
+		s.caConfigMapField = field
 	}
 }
 
 //updates Server parameters using ServerModifier functions. Once the manager is started these parameters cannot be updated
-func (s *Server) UpdateOpts(serverOpts ...ServerModifier) {
+func (s *Server) updateServerOpts(serverOpts ...ServerModifier) {
 	for _, serverOpt := range serverOpts {
-		serverOpt(s.webhookServer)
+		serverOpt(s)
 	}
 }
 
 func (s *Server) Start(stop <-chan struct{}) error {
 	s.log.Info("Starting nodenetworkconfigurationpolicy webhook server")
 
-	certManager, err := certificate.NewManager(s.mgr, s.webhookName, s.webhookType, s.webhookServer.CertDir, "tls.crt", "tls.key")
+	certManager, err := certificate.NewManager(s.mgr, s.webhookName, s.webhookType, s.webhookServer.CertDir, "tls.crt", "tls.key", s.caConfigMapKey, s.caConfigMapField)
 	if err != nil {
 		return errors.Wrap(err, "failed creating new webhook cert manager")
 	}
