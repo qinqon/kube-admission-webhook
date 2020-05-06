619c47169338e4fafa3b10a7469bf2a26027929d
Enrique Llorente Pastora <ellorent@redhat.com>
2020-05-06 13:27:15 +0200

    Replace CSR mechanism with self generated keys and self signed cert (#10)
    * Replace CSR mechanism with self generated ca/server key/cer
    
    The tls certificate rotation code is taken from [1] and the TLS primitives creation from kubevirt [2]
    
    [1] https://github.com/kubernetes/client-go/blob/master/util/certificate/certificate_manager.go
    [2] https://github.com/kubevirt/kubevirt/tree/master/pkg/certificates/triple
    
    This will work only with one master, for multi master leader election is needed
    
    
    * Run cert-manager as a nont LeaderElection controller-runtime Runnable
    
    This way at envs that has already the controller-runtime Manager configure with
    a leader election the cert-manager will run only at one pod and also controller-runtime
    do do the lifecycle of the cert-manager like the other controllers.
    
    * Add leaderElection aware controller
    
    The controlle-runtime v0.4.0 has all the controllers
    working as LeaderElection aware, at scenarios were is needed
    to run controllers without leader election they don't configure
    LeaderElection at manager so the cert-manager will run at all pods.
    
    For those scenarios a controller with a `WithoutLeaderElection` flag is
    implemented here while we merge [1] at controller-runtime.
    
    [1] https://github.com/kubernetes-sigs/controller-runtime/pull/921
    
    Extras:
    
    * Verify TLS cert
    * Add readyz probe
    * Load CACert from CABundle if cert-manager restarts

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # kube-admission-webhook
 k8s admission webhook library with certificate rotation and caBundle update.
+
+## CA/Service certificate/key generation
+The library generates RSA keys with 2048 size and certificate for both for CA and server.
+They share the expiration time so all the CA and service certificates
+are rotated at once just before expiration time.
+
+## Webhook service
+It has a one year expiration time harcoded and apart from wrapping the
+controller runtime webhook library it waits for TLS key/cert existence and
+correctness before sarting the service this wait cert-manager runs in parallel
+wait webhook just wait for proper TLS infra to be there.
+
+## Cert manager
+It's implemented as a controller runtime `Runnable` to be plug into the manager
+to re-use controller-runtime lifecycle code. The cert manager instance
+has to be unique per cluster so either is running a a `Deployment` with proper
+replication or use Leader Election at controller-runtime logic, if this is the
+case, in case the other controllers need to be non leader election a drop in
+place controller has beeing added to this project.
+
+## Examples
+There is a integration example under test/pod it contains two controllers and
+a webhook, one of the controllers uses leader election there other do not so
+all the bits from this project are represented.
--- pkg/webhook/server/server.go
+++ pkg/webhook/server/server.go
@@ -1,103 +1,163 @@
 package server
 
 import (
+	"crypto/x509"
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 
-	"k8s.io/apimachinery/pkg/types"
+	corev1 "k8s.io/api/core/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
-	certificate "github.com/qinqon/kube-admission-webhook/pkg/webhook/server/certificate"
+	"github.com/qinqon/kube-admission-webhook/pkg/webhook/server/certificate"
+	"github.com/qinqon/kube-admission-webhook/pkg/webhook/server/certificate/triple"
 )
 
 type Server struct {
-	mgr              manager.Manager
-	webhookName      string
-	webhookType      certificate.WebhookType
-	webhookServer    *webhook.Server
-	caConfigMapKey   types.NamespacedName
-	caConfigMapField string
-	log              logr.Logger
+	webhookServer *webhook.Server
+	certManager   *certificate.Manager
+	log           logr.Logger
 }
 
-type ServerModifier func(w *Server)
+type ServerModifier func(w *webhook.Server)
 
 // Add creates a new Conditions Mutating Webhook and adds it to the Manager. The Manager will set fields on the Webhook
 // and Start it when the Manager is Started.
-func New(mgr manager.Manager, webhookName string, webhookType certificate.WebhookType, serverOpts ...ServerModifier) *Server {
+func New(client client.Client, webhookName string, webhookType certificate.WebhookType, serverOpts ...ServerModifier) *Server {
 	s := &Server{
-		webhookName: webhookName,
-		webhookType: webhookType,
 		webhookServer: &webhook.Server{
 			Port:    8443,
 			CertDir: "/etc/webhook/certs/",
 		},
-		caConfigMapKey: types.NamespacedName{
-			Namespace: "kube-system",
-			Name:      "extension-apiserver-authentication",
-		},
-		caConfigMapField: "client-ca-file",
-		mgr:              mgr,
-		log:              logf.Log.WithName("webhook/server"),
+		certManager: certificate.NewManager(client, webhookName, webhookType, certificate.OneYearDuration),
+		log:         logf.Log.WithName("webhook/server"),
 	}
-	s.updateServerOpts(serverOpts...)
-
+	s.UpdateOpts(serverOpts...)
+	s.webhookServer.Register("/readyz", healthz.CheckHandler{Checker: healthz.Ping})
 	return s
 }
 
 func WithHook(path string, hook *webhook.Admission) ServerModifier {
-	return func(s *Server) {
-		s.webhookServer.Register(path, hook)
+	return func(s *webhook.Server) {
+		s.Register(path, hook)
 	}
 }
 
 func WithPort(port int) ServerModifier {
-	return func(s *Server) {
-		s.webhookServer.Port = port
+	return func(s *webhook.Server) {
+		s.Port = port
 	}
 }
 
 func WithCertDir(certDir string) ServerModifier {
-	return func(s *Server) {
-		s.webhookServer.CertDir = certDir
-	}
-}
-
-func WithCaConfigMapKey(key types.NamespacedName) ServerModifier {
-	return func(s *Server) {
-		s.caConfigMapKey = key
-	}
-}
-
-func WithCaConfigMapField(field string) ServerModifier {
-	return func(s *Server) {
-		s.caConfigMapField = field
+	return func(s *webhook.Server) {
+		s.CertDir = certDir
 	}
 }
 
 //updates Server parameters using ServerModifier functions. Once the manager is started these parameters cannot be updated
-func (s *Server) updateServerOpts(serverOpts ...ServerModifier) {
+func (s *Server) UpdateOpts(serverOpts ...ServerModifier) {
 	for _, serverOpt := range serverOpts {
-		serverOpt(s)
+		serverOpt(s.webhookServer)
 	}
+}
+
+func (s *Server) Add(mgr manager.Manager) error {
+	err := mgr.Add(s.certManager)
+	if err != nil {
+		return errors.Wrap(err, "failed adding certificate manager to controller-runtime manager")
+	}
+	err = mgr.Add(s)
+	if err != nil {
+		return errors.Wrap(err, "failed adding webhook server to controller-runtime manager")
+	}
+	return nil
+}
+
+func (s *Server) checkTLS() error {
+
+	_, err := os.Stat(path.Join(s.webhookServer.CertDir, corev1.TLSPrivateKeyKey))
+	if err != nil {
+		return errors.Wrap(err, "failed checking TLS key file stats")
+	}
+
+	_, err = os.Stat(path.Join(s.webhookServer.CertDir, corev1.TLSCertKey))
+	if err != nil {
+		return errors.Wrap(err, "failed checking TLS cert file stats")
+	}
+
+	key, err := ioutil.ReadFile(path.Join(s.webhookServer.CertDir, corev1.TLSPrivateKeyKey))
+	if err != nil {
+		return errors.Wrap(err, "failed reading for TLS key")
+	}
+
+	_, err = triple.ParsePrivateKeyPEM(key)
+	if err != nil {
+		return errors.Wrap(err, "failed parsing TLS key")
+	}
+
+	certPEM, err := ioutil.ReadFile(path.Join(s.webhookServer.CertDir, corev1.TLSCertKey))
+	if err != nil {
+		return errors.Wrap(err, "failed reading for TLS cert")
+	}
+
+	certs, err := triple.ParseCertsPEM(certPEM)
+	if err != nil {
+		return errors.Wrap(err, "failed parsing TLS cert")
+	}
+
+	caPEM, err := s.certManager.CABundle()
+	if err != nil {
+		return errors.Wrap(err, "failed to retrieve CA cert")
+	}
+
+	cas := x509.NewCertPool()
+	ok := cas.AppendCertsFromPEM([]byte(caPEM))
+	if !ok {
+		return errors.New("failed to parse CA certificate")
+	}
+
+	opts := x509.VerifyOptions{
+		Roots:   cas,
+		DNSName: certs[0].DNSNames[0],
+	}
+
+	if _, err := certs[0].Verify(opts); err != nil {
+		return errors.Wrap(err, "failed to verify certificate")
+	}
+
+	return nil
+}
+
+func (s *Server) waitForTLSReadiness() error {
+	return wait.PollImmediate(5*time.Second, 5*time.Minute, func() (bool, error) {
+		err := s.checkTLS()
+		if err != nil {
+			utilruntime.HandleError(err)
+			return false, nil
+		}
+		return true, nil
+	})
 }
 
 func (s *Server) Start(stop <-chan struct{}) error {
 	s.log.Info("Starting nodenetworkconfigurationpolicy webhook server")
 
-	certManager, err := certificate.NewManager(s.mgr, s.webhookName, s.webhookType, s.webhookServer.CertDir, "tls.crt", "tls.key", s.caConfigMapKey, s.caConfigMapField)
+	err := s.waitForTLSReadiness()
 	if err != nil {
-		return errors.Wrap(err, "failed creating new webhook cert manager")
+		return errors.Wrap(err, "failed watting for ready TLS key/cert")
 	}
-
-	err = certManager.Start()
-	if err != nil {
-		return errors.Wrap(err, "failed starting webhook cert manager")
-	}
-	defer certManager.Stop()
 
 	err = s.webhookServer.Start(stop)
 	if err != nil {
