0731a8ddc381eddeb37329a1a94056f0e381c853
Quique Llorente <ellorent@redhat.com>
2020-06-30 12:45:39 +0200

    Test controller to check if Reconcile kicks in after secret or webhook config is changed
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- pkg/certificate/controller_test.go
+++ pkg/certificate/controller_test.go
@@ -9,7 +9,9 @@ import (
 
 	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
@@ -22,10 +24,30 @@ var (
 
 var _ = Describe("Certificates controller", func() {
 	var (
-		certsDuration = 256 * 24 * time.Hour
-		mgr           *Manager
-		now           time.Time
+		certsDuration           = 256 * 24 * time.Hour
+		mgr                     *Manager
+		now                     time.Time
+		isTLSEventuallyVerified = func() AsyncAssertion {
+			return Eventually(func() error {
+				return mgr.verifyTLS()
+			}, 5*time.Second, 1*time.Second)
+		}
+
+		isTLSSecretEventuallyPresent = func() AsyncAssertion {
+			return Eventually(func() (bool, error) {
+				obtainedSecret := corev1.Secret{}
+				err := cli.Get(context.TODO(), types.NamespacedName{Namespace: expectedSecret.Namespace, Name: expectedSecret.Name}, &obtainedSecret)
+				if err != nil {
+					if apierrors.IsNotFound(err) {
+						return false, nil
+					}
+					return false, err
+				}
+				return true, nil
+			}, 5*time.Second, 1*time.Second)
+		}
 	)
+
 	BeforeEach(func() {
 
 		mgr = NewManager(cli, "foowebhook", MutatingWebhook, certsDuration)
@@ -35,6 +57,7 @@ var _ = Describe("Certificates controller", func() {
 
 		createResources()
 	})
+
 	AfterEach(func() {
 		deleteResources()
 	})
@@ -113,4 +136,82 @@ var _ = Describe("Certificates controller", func() {
 			})
 		})
 	})
+	Context("when integrated into a controller-runtime manager and started", func() {
+		var (
+			crManager manager.Manager
+			stopCh    chan struct{}
+		)
+		BeforeEach(func(done Done) {
+
+			By("Creating new controller-runtime manager")
+			var err error
+			crManager, err = manager.New(testEnv.Config, manager.Options{Namespace: expectedNamespace.Name})
+			Expect(err).ToNot(HaveOccurred(), "should success creating controller-runtime manager")
+
+			err = mgr.Add(crManager)
+			Expect(err).To(Succeed(), "should succeed adding the cert manager controller to the controller-runtime manager")
+
+			By("Starting controller-runtime manager")
+			stopCh = make(chan struct{})
+			go func() {
+				defer GinkgoRecover()
+				err = crManager.Start(stopCh)
+				Expect(err).To(Succeed(), "should success starting manager")
+			}()
+
+			By("Checking that the TLS secret is created")
+			isTLSSecretEventuallyPresent().Should(BeTrue(), "should eventually have the TLS secret")
+			close(done)
+
+			By("Wait a little for Reconcile to settle after mutatingwebhook configuration reconcile")
+			time.Sleep(3 * time.Second)
+		}, 10)
+		AfterEach(func() {
+			close(stopCh)
+		})
+		Context("and TLS secret is deleted", func() {
+			BeforeEach(func() {
+				By("Delete the TLS secret")
+				err := cli.Delete(context.TODO(), &expectedSecret)
+				Expect(err).To(Succeed(), "should succeed deleteing TLS secret")
+				isTLSSecretEventuallyPresent().Should(BeFalse(), "should eventually delete the TLS secret")
+			})
+			It("should re-create TLS secret", func() {
+				isTLSEventuallyVerified().Should(Succeed(), "should eventually have a TLS secret")
+			})
+		})
+		Context("and CABundle is reset", func() {
+			BeforeEach(func() {
+				obtainedWebhookConfiguration := getWebhookConfiguration()
+				obtainedWebhookConfiguration.Webhooks[0].ClientConfig.CABundle = []byte{}
+				updateWebhookConfiguration(obtainedWebhookConfiguration)
+			})
+			It("should re-create CABundle", func() {
+				isTLSEventuallyVerified().Should(Succeed(), "should eventually have a TLS secret")
+			})
+		})
+
+	})
 })
+
+func getSecret() corev1.Secret {
+	obtainedSecret := corev1.Secret{}
+	err := cli.Get(context.TODO(), types.NamespacedName{Namespace: expectedSecret.Namespace, Name: expectedSecret.Name}, &obtainedSecret)
+	Expect(err).To(Succeed(), "should succeed getting TLS secret")
+	return obtainedSecret
+}
+
+func getWebhookConfiguration() admissionregistrationv1beta1.MutatingWebhookConfiguration {
+	obtainedWebhookConfiguration := admissionregistrationv1beta1.MutatingWebhookConfiguration{}
+	err := cli.Get(context.TODO(), types.NamespacedName{
+		Namespace: expectedMutatingWebhookConfiguration.Namespace,
+		Name:      expectedMutatingWebhookConfiguration.Name,
+	}, &obtainedWebhookConfiguration)
+	Expect(err).To(Succeed(), "should succeed getting mutating webhook configuration")
+	return obtainedWebhookConfiguration
+}
+
+func updateWebhookConfiguration(webhookConfiguration admissionregistrationv1beta1.MutatingWebhookConfiguration) {
+	err := cli.Update(context.TODO(), &webhookConfiguration)
+	Expect(err).To(Succeed(), "should succeed update mutatingwebhookconfiguration")
+}
