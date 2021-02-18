ab890468d65ab87e8661d1e9ab79c65834c124d9
Quique Llorente <ellorent@redhat.com>
2021-02-18 16:00:28 +0100

    Group imports
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- pkg/certificate/cleanup.go
+++ pkg/certificate/cleanup.go
@@ -6,10 +6,9 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 
 	corev1 "k8s.io/api/core/v1"
-
-	"github.com/pkg/errors"
 
 	"github.com/qinqon/kube-admission-webhook/pkg/certificate/triple"
 )
