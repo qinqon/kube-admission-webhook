39a1ee6ee25cb8cc792df9765fe7ab4d9957d7c4
Ram Lavi <ralavi@redhat.com>
2021-10-05 21:15:05 +0300

    certs, Add Certs list size limit
    Currently certs are appended without limit. This can
    cause a problem when applying the caBundle/certs on the
    mutatingwebhookconfiguration/secret, appropriately - due
    to etcd's POST size limit.
    Limited the amount of certs to 100.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/certificate/triple/triple_test.go
+++ pkg/certificate/triple/triple_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 )
 
@@ -42,4 +43,46 @@ var _ = Describe("Cert library", func() {
 		})
 
 	})
+
+	type removeOldestCertsParams struct {
+		certsList         []*x509.Certificate
+		maxListSize       int
+		expectedCertsList []*x509.Certificate
+	}
+	certOldest := &x509.Certificate{
+		NotBefore: time.Now().Add(10*time.Hour),
+		NotAfter:  time.Now(),
+	}
+	certOld := &x509.Certificate{
+		NotBefore: time.Now().Add(5*time.Hour),
+		NotAfter:  time.Now(),
+	}
+	certCurrent := &x509.Certificate{
+		NotBefore: time.Now(),
+		NotAfter:  time.Now(),
+	}
+
+	DescribeTable("removeOldestCerts",
+		func(c removeOldestCertsParams) {
+			Expect(removeOldestCerts(c.certsList, c.maxListSize)).To(ConsistOf(c.expectedCertsList), "should remove the oldest certs")
+		},
+		Entry("when list is empty",
+			removeOldestCertsParams{
+			certsList:         []*x509.Certificate{},
+			maxListSize:       2,
+			expectedCertsList: []*x509.Certificate{},
+			}),
+		Entry("when list size is less or equal to max certs, should keep the certs list intact",
+			removeOldestCertsParams{
+				certsList:         []*x509.Certificate{certCurrent, certOld, certOldest},
+				maxListSize:        3,
+				expectedCertsList: []*x509.Certificate{certCurrent, certOld, certOldest},
+			}),
+		Entry("when list size is bigger than max certs, should remove the oldest certs",
+			removeOldestCertsParams{
+				certsList:         []*x509.Certificate{certCurrent, certOld, certOldest},
+				maxListSize:       2,
+				expectedCertsList: []*x509.Certificate{certCurrent, certOld},
+			}),
+	)
 })
